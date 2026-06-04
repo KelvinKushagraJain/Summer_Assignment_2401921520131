390e0b7839ded1622a8d1936df7244595901486f
Kelvin Kushagra Jain <24112cn236@glbitm.ac.in>
2026-06-04 23:07:15 +0530

    Solution for move zeroes problem

---
+++ Week_1/Day_3/Move_Zeroes.java
@@ -0,0 +1,24 @@
+ class Solution {
+    public void moveZeroes(int[] arr) {
+
+        int pos = 0;
+
+        for (int i = 0; i < arr.length; i++) 
+        {
+
+            if (arr[i] != 0) 
+            {
+                arr[pos] = arr[i];
+                
+                pos = pos +1;
+            }
+        }
+
+        while (pos < arr.length) 
+        {
+            arr[pos] =0;
+
+            pos = pos +1;
+        }
+    }
+}
